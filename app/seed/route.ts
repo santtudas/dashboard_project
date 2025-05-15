3fa44cfdacd527cff124ee334e12f5cc57586aa3
santtudas <santeri.pulliainen@student.laurea.fi>
2025-05-15 14:33:26 +0300

    Comment off route.ts

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,3 +1,4 @@
+/**
 import bcrypt from 'bcryptjs';
 import postgres from 'postgres';
 import { invoices, customers, revenue, users } from '../lib/placeholder-data';
@@ -115,3 +116,4 @@ export async function GET() {
     return Response.json({ error }, { status: 500 });
   }
 }
+*/
